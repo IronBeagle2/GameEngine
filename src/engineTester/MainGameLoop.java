13ac6ab907b8466717e8a68b32c32334e2054dea
IronBeagle2 <63589160+IronBeagle2@users.noreply.github.com>
2020-11-09 18:56:51 +0100

    Custom OBJ support
    yes

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -9,6 +9,7 @@ import models.RawModel;
 import models.TexturedModel;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
+import renderEngine.OBJLoader;
 import renderEngine.Renderer;
 import shaders.StaticShader;
 import textures.ModelTexture;
@@ -34,7 +35,7 @@ public class MainGameLoop {
 		 */
 
 		// 3D
-		float[] vertices = { -0.5f, 0.5f, -0.5f, -0.5f, -0.5f, -0.5f, 0.5f, -0.5f, -0.5f, 0.5f, 0.5f, -0.5f,
+		/*float[] vertices = { -0.5f, 0.5f, -0.5f, -0.5f, -0.5f, -0.5f, 0.5f, -0.5f, -0.5f, 0.5f, 0.5f, -0.5f,
 
 				-0.5f, 0.5f, 0.5f, -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f, 0.5f, 0.5f, 0.5f,
 
@@ -58,20 +59,20 @@ public class MainGameLoop {
 		int[] indices = { 0, 1, 3, 3, 1, 2, 4, 5, 7, 7, 5, 6, 8, 9, 11, 11, 9, 10, 12, 13, 15, 15, 13, 14, 16, 17, 19,
 				19, 17, 18, 20, 21, 23, 23, 21, 22
 
-		};
+		};*/
 
-		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
-		TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("texture3")));
+		RawModel model = OBJLoader.loadObjModel("dragon", loader);
+		TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("debug_big")));
 		// ModelTexture texture = new ModelTexture(loader.loadTexture("texture"));
 		// TexturedModel texturedModel = new TexturedModel(model, texture);
 
-		Entity entity = new Entity(staticModel, new Vector3f(0, 0, -1f), 0, 0, 0, 1);
+		Entity entity = new Entity(staticModel, new Vector3f(0, -5f, 0), 0, 0, 0, 1);
 
 		Camera camera = new Camera();
 
 		while (!Display.isCloseRequested()) {
-			// entity.increasePosition(0.002f, 0, 0);
-			entity.increaseRotation(1, 1, 1);
+			entity.increasePosition(0, 0, 0);
+			entity.increaseRotation(0, 1, 0);
 			camera.move();
 			renderer.prepare();
 			shader.start();
